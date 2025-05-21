5a203397c9cfe9efd20bf9c68d8d8e64e3580cbb
shahzad <shahzadakhtar182005@gmail.com>
2025-05-21 12:53:02 +0530

    feat(websocket): validate sec-websocket-accept key

--- websocket.c
+++ websocket.c
@@ -3,7 +3,9 @@
 #include "libs/base64.h"
 #include "libs/dyn_array.h"
 #include "libs/rng_xoroshiro.h"
+#include "libs/sha1.h"
 #include "libs/utils.h"
+#include <stddef.h>
 #include <stdint.h>
 
 #ifndef BS_IMPLEMENTATION
@@ -46,23 +48,24 @@ static _httpHeader *_http_payload_header_get(_httpPayload *payload,
 // managed or nah, that's the question
 static inline void _http_payload_append_header(_httpPayload *payload,
                                                const _httpHeader http_header) {
-  ALLOCATOR_STATUS result = ALLOCATOR_SUCCESS;
+  _httpHeader *result = NULL;
   da_append(*payload, http_header, result);
+  assert(result != NULL);
 }
 static inline ALLOCATOR_STATUS _http_parse_header(_httpPayload *payload,
                                                   BetterString_View *request) {
   // todo(shahzad)!: use arena allocator
   if (payload->http_line.view == NULL || payload->http_line.len == 0) {
     BetterString_View http_line = bs_split_once_by_bs(request, BSV("\r\n"));
-    ALLOCATOR_STATUS _err =
-        bs_clone(payload->allocator_ctx, http_line, &payload->http_line);
-    if (_err != ALLOCATOR_SUCCESS) {
-      return _err;
+
+    payload->http_line = bs_clone(payload->allocator_ctx, http_line);
+    if (payload->http_line.view == NULL) {
+      return ALLOCATOR_OUT_OF_MEMORY;
     }
     _LOG_DEBUG("http line: " BSV_Fmt "\n", BSV_Arg(http_line));
   }
   while (request->len > 0) {
-    const BetterString_View http_header =
+    BetterString_View http_header =
         bs_trim(bs_split_once_by_bs(request, BSV("\r\n")));
     if (http_header.len == 0) {
       break;
@@ -86,6 +89,9 @@ static inline ALLOCATOR_STATUS _http_parse_header(_httpPayload *payload,
     _http_payload_append_header(
         payload, (_httpHeader){.key = bs_builder_to_sv(&owned_key),
                                .value = bs_builder_to_sv(&owned_value)});
+    _LOG_DEBUG("key: \"" BSV_Fmt "\"\n", BSV_Arg(key));
+    _LOG_DEBUG("value: \"" BSV_Fmt "\"\n", BSV_Arg(value));
+    _LOG_DEBUG("\n");
   }
   return ALLOCATOR_SUCCESS;
 }
@@ -138,26 +144,51 @@ static ssize_t ws_read_tcp_data(Ws_Context *ctx, uint8_t *buff, size_t len) {
 }
 
 static bool ws_verify_websocket_sec_key(Ws_Context *ctx,
-                                        BetterString_View sec_key) {
-
+                                        BetterString_View sec_key,
+                                        BetterString_View sec_accept) {
   // todo(shahzad)!: fix this please
-  const size_t min_length = BASE64_ENCODE_MIN_OUTPUT_BUFFER_SIZE(sec_key.len);
-  BetterString_Builder i_should_stop_raw_dogging_this =
-      bs_builder_new(ctx->allocator_ctx.ctx);
-  ALLOCATOR_STATUS status =
-      bs_builder_reserve_exact(&i_should_stop_raw_dogging_this, min_length);
+  printf("\n");
+  BetterString_Builder sec_key_builder = bs_builder_new(ctx);
+  ALLOCATOR_STATUS status = ALLOCATOR_SUCCESS;
+  bs_builder_sprintf(status, &sec_key_builder, BSV_Fmt "%s", BSV_Arg(sec_key),
+                     "258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
   assert(status == ALLOCATOR_SUCCESS);
-  base64_decode(sec_key.view, sec_key.len,
-                i_should_stop_raw_dogging_this.string.data,
-                i_should_stop_raw_dogging_this.string.capacity,
-                &i_should_stop_raw_dogging_this.string.len);
+  sha1Digest digest = sha1_digest(ctx->allocator_ctx.ctx,
+                                  (const uint8_t *)sec_key_builder.string.data,
+                                  sec_key_builder.string.len);
 
-  for (size_t i = 0; i < sec_key.len; i++) {
-    printf("\\x%x", (uint8_t)sec_key.view[i]);
+  assert(memcmp(digest._As.u32, (uint32_t[5]){0}, 5) != 0);
+
+  sha1_print_digest(&digest);
+  digest = sha1_to_le(digest);
+
+  const uint8_t *sha1_digest_array = digest._As.u8;
+  const size_t sha1_digest_array_len = sizeof(digest._As.u8);
+  for (size_t i = 0; i < sha1_digest_array_len; i++) {
+    printf("%x", sha1_digest_array[i]);
   }
   printf("\n");
 
-  _TODO("implement sha1 hash");
+  assert(sha1_digest_array_len == 5 * 4);
+
+  const size_t sha1_base64_required_len =
+      BASE64_ENCODE_REQUIRED_OUTPUT_BUFFER_SIZE(sha1_digest_array_len + 2);
+
+  BetterString_Builder base64_digest_builder =
+      bs_builder_new_alloc(ctx->allocator_ctx.ctx, sha1_base64_required_len);
+
+  base64_encode(sha1_digest_array, sha1_digest_array_len,
+                base64_digest_builder.string.data,
+                base64_digest_builder.string.capacity,
+                &base64_digest_builder.string.len,
+                (base64EncodeSettings){.padding = BASE64_ENCODE_PADDING});
+  const BetterString_View base64_digest_sv =
+      bs_builder_to_sv(&base64_digest_builder);
+  WS_LOG_DEBUG("sec_key: \"" BSV_Fmt "\"\n", BSV_Arg(base64_digest_sv));
+  WS_LOG_DEBUG("sec_sec_accept: \"" BSV_Fmt "\"\n", BSV_Arg(sec_accept));
+  assert(bs_eq(base64_digest_sv, sec_accept));
+
+  return true;
 }
 void ws_do_http_upgrade(Ws_Context *ctx) {
   BetterString_Builder upgrade_request = {0};
@@ -168,21 +199,24 @@ void ws_do_http_upgrade(Ws_Context *ctx) {
   uint8_t sec_websocket_key[16] = {0};
   xoroshiro128Ctx rng_ctx = xoroshiro128_init((size_t)time(NULL));
   xoroshiro128_fill(&rng_ctx, sec_websocket_key, 16);
-  char sec_websocket_key_base64[64] = {0};
-  size_t encoded_sec_websocket_key_len = 0;
+  BetterString_ViewManaged sec_websocket_key_base64 = {.view = (char[64]){0},
+                                                       .len = 0};
   BASE64_STATUS base64_status =
-      base64_encode(sec_websocket_key, 16, sec_websocket_key_base64, 64,
-                    &encoded_sec_websocket_key_len,
+      base64_encode(sec_websocket_key, 16, sec_websocket_key_base64.view, 64,
+                    &sec_websocket_key_base64.len,
                     (base64EncodeSettings){.padding = BASE64_ENCODE_PADDING});
   assert(base64_status == BASE64_STATUS_SUCCESS);
   ALLOCATOR_STATUS builder_status = ALLOCATOR_SUCCESS;
+  WS_LOG_DEBUG("sec key original: \"" BSV_Fmt "\"\n",
+               BSV_Arg(sec_websocket_key_base64));
 
   bs_builder_append_cstr(&upgrade_request, "GET / HTTP/1.1\r\n");
   bs_builder_append_cstr(&upgrade_request, "Host: 127.0.0.1\r\n");
   bs_builder_append_cstr(&upgrade_request, "Upgrade: websocket\r\n");
   bs_builder_append_cstr(&upgrade_request, "Connection: Upgrade\r\n");
   bs_builder_sprintf(builder_status, &upgrade_request,
-                     "Sec-WebSocket-Key: %s\r\n", sec_websocket_key_base64);
+                     "Sec-WebSocket-Key: %s\r\n",
+                     sec_websocket_key_base64.view);
   bs_builder_append_cstr(&upgrade_request, "Origin: http://127.0.0.1\r\n");
   bs_builder_append_cstr(&upgrade_request, "Sec-WebSocket-Protocol: chat\r\n");
   bs_builder_append_cstr(&upgrade_request, "Sec-WebSocket-Version: 13\r\n");
@@ -213,5 +247,7 @@ void ws_do_http_upgrade(Ws_Context *ctx) {
   }
   _httpHeader *response_sec_key =
       _http_payload_header_get(&payload, BSV("Sec-WebSocket-Accept"));
-  ws_verify_websocket_sec_key(ctx, response_sec_key->value);
+  ws_verify_websocket_sec_key(ctx,
+                              *(BetterString_View *)&sec_websocket_key_base64,
+                              response_sec_key->value);
 }
