743c81a07d5fcee99acfd8b989c32dd536d345f2
shahzad <shahzadakhtar182005@gmail.com>
2025-08-09 17:15:36 +0530

    io_uring is hard so we switching to blocking reads

--- io/unix_internal.c
+++
@@ -1,9 +0,0 @@
-#include <fcntl.h>
-
-static inline int unix__enable_nonblocking_io(int fd) {
-  const int current_flags = fcntl(fd, F_GETFL);
-  if (current_flags == -1) {
-    return -1;
-  }
-  return fcntl(fd, F_SETFL, current_flags | O_NONBLOCK);
-}
